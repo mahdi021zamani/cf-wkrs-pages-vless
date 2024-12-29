8dacc422bfc9f0b9d4c1a9c539fe1a16190ba301
mahdi021zamani <167538602+mahdi021zamani@users.noreply.github.com>
2024-12-29 15:01:54 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a05397b5-261f-4b1c-aee0-8b23f7611d42';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
